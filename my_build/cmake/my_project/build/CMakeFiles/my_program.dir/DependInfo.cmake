2dabaaf120008894ac55ed90d4d75d06fd04a02c
jhyunleehi <jhyunleehi@naver.com>
2025-03-14 17:26:32 +0900

    cmake

---
+++ my_build/cmake/my_project/build/CMakeFiles/my_program.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jhyunlee/code/3FS/my_build/cmake/my_project/src/main.cpp" "CMakeFiles/my_program.dir/src/main.cpp.o" "gcc" "CMakeFiles/my_program.dir/src/main.cpp.o.d"
+  "/home/jhyunlee/code/3FS/my_build/cmake/my_project/src/math.cpp" "CMakeFiles/my_program.dir/src/math.cpp.o" "gcc" "CMakeFiles/my_program.dir/src/math.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ my_build/cmake/my_project/build/CMakeFiles/my_program.dir/src/math.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/my_program.dir/src/math.cpp.o: \
+ /home/jhyunlee/code/3FS/my_build/cmake/my_project/src/math.cpp \
+ /usr/include/stdc-predef.h \
+ /home/jhyunlee/code/3FS/my_build/cmake/my_project/src/math.h
